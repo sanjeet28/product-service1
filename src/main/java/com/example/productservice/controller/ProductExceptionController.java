310c150fdd7a172feed5ca118e2bd232818bdc9b
sanjeet91 <you@example.com>
2022-10-05 11:59:11 +0530

    changes

---
+++ src/main/java/com/example/productservice/controller/ProductExceptionController.java
@@ -0,0 +1,16 @@
+package com.example.productservice.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.productservice.exceptions.ProductNotFoundException;
+
+@ControllerAdvice
+public class ProductExceptionController {
+	@ExceptionHandler(value=ProductNotFoundException.class)
+	public ResponseEntity<Object> handleException(ProductNotFoundException ex) {
+		return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.NOT_FOUND);
+
+}
+}
---
+++ src/main/java/com/example/productservice/exceptions/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.productservice.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+	public ProductNotFoundException(int id) {
+		super("Product with id " + id + " not found.");
+
+}
+}
